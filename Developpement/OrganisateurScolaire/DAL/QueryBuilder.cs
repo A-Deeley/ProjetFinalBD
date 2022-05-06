28eae05576ba56116777321fa042e76591eeeef9
Andrew Deeley <adeeley98@gmail.com>
2022-05-06 10:56:56 -0400

    Creation d'un querybuilder

---
+++ Developpement/OrganisateurScolaire/DAL/QueryBuilder.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganisateurScolaire.DAL
+{
+    public class QueryBuilder
+    {
+        private readonly MySqlCommand _command;
+
+        private QueryBuilder(MySqlConnection connection)
+        {
+            _command.Connection = connection;
+        }
+
+        public static QueryBuilder Init(MySqlConnection connection)
+        {
+            return new QueryBuilder(connection);
+        }
+        
+        public QueryBuilder AddParameterWithValue(string parameterName, object value)
+        {
+            _command.Parameters.AddWithValue(parameterName, value);
+            return this;
+        }
+
+        public QueryBuilder SetQuery(string query)
+        {
+            _command.CommandText = query;
+            return this;
+        }
+
+        public MySqlCommand Build() => _command;
+    }
+}
