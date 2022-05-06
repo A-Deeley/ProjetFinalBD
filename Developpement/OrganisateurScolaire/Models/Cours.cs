b82ab119128199a9a7dc1777b55f748ed77681b6
Andrew Deeley <adeeley98@gmail.com>
2022-05-06 10:37:58 -0400

    Ajouter attributs et champs dans les models.

--- Developpement/OrganisateurScolaire/Models/Cours.cs
+++ Developpement/OrganisateurScolaire/Models/Cours.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace OrganisateurScolaire.Models
 {
@@ -11,5 +12,9 @@ namespace OrganisateurScolaire.Models
     /// </summary>
     public class Cours
     {
+        public string Numero { get; private set; }
+        public string Nom { get; private set; }
+        public string Description { get; private set; }
+        public Brush Couleur { get; private set; }
     }
 }
--- Developpement/OrganisateurScolaire/Models/Tache.cs
+++ Developpement/OrganisateurScolaire/Models/Tache.cs
@@ -11,5 +11,12 @@ namespace OrganisateurScolaire.Models
     /// </summary>
     public class Tache
     {
+        public int ID { get; private set; }
+        public Cours Cours { get; private set; }
+        public Statut Statut { get; private set; }
+        public string Titre { get; private set; }
+        public DateTime DateDebut { get; private set; }
+        public DateTime DateFin { get; private set; }
+        public string Description { get; private set; }
     }
 }
